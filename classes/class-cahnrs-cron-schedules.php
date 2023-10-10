f70a631c0a96681e23a52059d76b01e52a5f8286
Efren Vasquez <efren.vasquez@wsu.edu>
2023-10-09 21:27:05 -0500

    Add capablity of adding custom messages in email.

--- classes/class-cahnrs-cron-schedules.php
+++ classes/class-cahnrs-cron-schedules.php
@@ -21,18 +21,18 @@ class CAHNRSEmailCronSchedule {
     
     //Emails user a report if there is at least one issue, warning, or alert. 
     public function cahnrs_accessibility_report_email_cron(){
-        include 'class-cahnrs-accessibility-query.php';
+        $cahnrs_accessibility_query = new CAHNRSAccessibilityQuery();
+        $cahnrs_accessibility_query = $cahnrs_accessibility_query->cahnrs_report_query();
 
-        $accessibilityQuery = new CAHNRSAccessibilityQuery();
-        $errorData = $accessibilityQuery->cahnrs_report_query();
-        $totalErrors = $errorData['total_errors'];
-        $totalWarnings = $errorData['total_warnings'];
-        $totalAlerts = $errorData['total_alerts'];
-
+        $total_errors = $cahnrs_accessibility_query['total_errors'];
+        $total_alerts = $cahnrs_accessibility_query['total_alerts'];
+        $total_warnings = $cahnrs_accessibility_query['total_warnings'];
+            
         $report_content = $this->cahnrs_generate_report_content(); 
-        $report_email = get_option('report_email', '');
+        $report_email = get_option('selected_recipients', '');
+        $report_email = implode(',', $report_email);
         
-        if ($totalErrors > 0 || $totalWarnings > 0 || $totalAlerts > 0) {
+        if ($total_errors > 0 || $total_alerts > 0 || $total_warnings > 0) {
             $subject = 'Accessibility Report';
             $message = $report_content;
             $current_date = current_datetime()->format('m-d-Y H:i:s A');
@@ -40,35 +40,38 @@ class CAHNRSEmailCronSchedule {
             
             wp_mail($report_email, $subject, $message);
         }
-
         
     }
 
     //Generates content for email.
-    public function cahnrs_generate_report_content() {
+    public static function cahnrs_generate_report_content() {
 
         $content = '';
         $cahnrs_admin_report_page = get_site_url() . '/wp-admin/admin.php?page=cahnrs-accessibility';
         $cahnrs_site_title = get_bloginfo('name');
+        $custom_email_content = get_option('custom_email_content', '');
 
-        $content .= "<p>Hello, <br> Your website $cahnrs_site_title has some accessibility issues that need to be looked at. Please visit the link below to make changes to your website.</p>";
+        if (!empty($custom_email_content)) {
+            $content = $custom_email_content; 
+        }else {
+            $content .= "<p>Hello, <br> Your website $cahnrs_site_title has some accessibility issues that need to be looked at. Please visit the link below to make changes to your website.</p>";
 
-        $content .= "<p><a href='$cahnrs_admin_report_page'>View Accessibility Report</a></p> " ;
-
-        $content .= "<p>If you have questions about this report, please contact the CAHNRS Web Team. If you need assistance fixing the accessibility issues on your site, please fill out the web support form or refer to the list of web accessibility resources below:</p> " ;
-
-        $content .= "<ul>
-                        <li><a href='https://communications.cahnrs.wsu.edu/web-services/training/'>Virtual Open Lab with the CAHNRS web team</a></li>
-                        <li><a href='https://communications.cahnrs.wsu.edu/web-services/training/web-accessibility-resources/'>Web Accessibility Resources</a></li>
-                        <li><a href='https://web.wsu.edu/web-accessibility/accessibility-guides/'>Accessibility Guides</a></li>
-                        <li><a href='https://wave.webaim.org/'>WAVE Web Accessibility Evaluation Tools</a></li>
-                        <li><a href='https://web.wsu.edu/web-accessibility/accessibility-guides/creating-an-accessible-microsoft-word-document/'>Creating an Accessible Word Document</a></li>
-                        <li><a href='https://web.wsu.edu/web-accessibility/accessibility-guides/creating-an-accessible-pdf/'>Creating and Accessible PDF</a></li>
-                        <li><a href='https://web.wsu.edu/web-accessibility/web-accessibility-training/'>Required WSU Web Accessibility Training</a></li>
-                    </ul>";
-
-        $content .= "<p>Thank you, <br> CAHNRS Web Team</p>";
+            $content .= "<p><a href='$cahnrs_admin_report_page'>View Accessibility Report</a></p> " ;
     
+            $content .= "<p>If you have questions about this report, please contact the CAHNRS Web Team. If you need assistance fixing the accessibility issues on your site, please fill out the web support form or refer to the list of web accessibility resources below:</p> " ;
+    
+            $content .= "<ul>
+                            <li><a href='https://communications.cahnrs.wsu.edu/web-services/training/'>Virtual Open Lab with the CAHNRS web team</a></li>
+                            <li><a href='https://communications.cahnrs.wsu.edu/web-services/training/web-accessibility-resources/'>Web Accessibility Resources</a></li>
+                            <li><a href='https://web.wsu.edu/web-accessibility/accessibility-guides/'>Accessibility Guides</a></li>
+                            <li><a href='https://wave.webaim.org/'>WAVE Web Accessibility Evaluation Tools</a></li>
+                            <li><a href='https://web.wsu.edu/web-accessibility/accessibility-guides/creating-an-accessible-microsoft-word-document/'>Creating an Accessible Word Document</a></li>
+                            <li><a href='https://web.wsu.edu/web-accessibility/accessibility-guides/creating-an-accessible-pdf/'>Creating and Accessible PDF</a></li>
+                            <li><a href='https://web.wsu.edu/web-accessibility/web-accessibility-training/'>Required WSU Web Accessibility Training</a></li>
+                        </ul>";
+    
+            $content .= "<p>Thank you, <br> CAHNRS Web Team</p>";
+        }
 
         return $content;
     }
